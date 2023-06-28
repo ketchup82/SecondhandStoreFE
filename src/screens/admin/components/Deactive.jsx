d00e0c2846ad3718bbff3d02a540e602fabd89c9
ketchup <136414297+ketchup82@users.noreply.github.com>
2023-06-28 21:41:35 +0700

    Update Deactive.jsx

--- src/screens/admin/components/Deactive.jsx
+++ src/screens/admin/components/Deactive.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export const Deactive = () => {
     return (
         <div className='p-5'>
-            <button type="button" className="btn btn-light fw-medium text-uppercase mb-5">←Back</button>
+            <button type="button" className="btn btn-light fw-medium text-uppercase mb-5">
+            <Link to="/admin/admin-home">←Back</Link>
+                </button>
             <h1 className='text-center'>ARE YOU SURE TO DEACTIVATE USER WITH ID ??? </h1>
             <div className="d-flex justify-content-center">
                 <button className='btn btn-success m-3'>Yes</button>
